dc283c0204a1bb3a9ffc294f88695a2b6a4dbb7e
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-17 17:25:04 -0500

    IIFE

---
+++ iife.js
@@ -0,0 +1,11 @@
+(function(){
+    const superHero = "Batman"
+    console.log(superHero)
+})();
+
+(function(){
+    const superHero = "Superman"
+    console.log(superHero)
+})();
+
+// Immediately Invoked Function Expression (IIFE) in Node.js
