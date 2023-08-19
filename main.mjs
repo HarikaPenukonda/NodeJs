8a8cddebfd5ee385d60e12ccb085169717ddca26
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 14:21:15 -0500

    ES module import-export pattern-4 type-2

--- main.mjs
+++ main.mjs
@@ -1,6 +1,7 @@
-import * as math from './math-esm.mjs'
+//import * as math from './math-esm.mjs'
+import {add,subract} from './math-esm.mjs'
 
-const {add,subract} = math
+//const {add,subract} = math
 
 console.log(add(5,5))
 console.log(subract(5,5))
