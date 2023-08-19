404c81571134aa7e0be91d71f70f85768e153daf
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 16:37:57 -0500

    with using events module

--- built-in-modules/events/main.js
+++ built-in-modules/events/main.js
@@ -2,5 +2,12 @@ const PizzaShop  = require("./pizza-shop")
 
 const pizzaShop = new PizzaShop()
 
-pizzaShop.order()
+pizzaShop.on("order",(size,topping)=>{
+    console.log(`order-recieved!, Baking a ${size} pizza with ${topping}`)
+})
+
+pizzaShop.order("large","pineapple")
+pizzaShop.displayOrderNumber()
+
+pizzaShop.order("small","mushroom")
 pizzaShop.displayOrderNumber()
