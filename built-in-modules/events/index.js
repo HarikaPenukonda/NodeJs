1d648948f225d94838010e89cf0c8545f3835916
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 16:11:12 -0500

    events module

--- built-in-modules/events/index.js
+++ built-in-modules/events/index.js
@@ -7,5 +7,12 @@ emitter.on("order-pizza",(size,topping)=>{
     console.log(`order-recieved!, Baking a ${size} pizza with ${topping}`)
 })
 
+emitter.on("order-pizza", (size)=>{
+    if(size === "large"){
+        console.log("Serving complimentary drink")
+    }
+})
+
+console.log("Do work before event occurs in the system")
 emitter.emit("order-pizza","large","mushroom")
 
