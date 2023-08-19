26b02eee84787f466bc9721c566511c116a1c92f
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 13:26:52 -0500

    assign the object literal

--- object-reference.js
+++ object-reference.js
@@ -2,9 +2,19 @@ const obj1 = {
     name : "Bruce wayne"
 }
 
-const obj2 = obj1;
-obj2.name = "Clark kent"
+// const obj2 = obj1;
+// obj2.name = "Clark kent"
 
-console.log(obj1) // { name: 'Clark kent' }
+//console.log(obj1) // { name: 'Clark kent' }
 // when you assign an object to another, both objects point at the same address in memory. Modifying one will lead
 // to another
+
+let obj2 = obj1
+obj2 = {
+    name : "Clark kent"
+}
+
+console.log(obj1) // { name: 'Bruce wayne' }
+/*
+    when we assign the object literal, the reference was broken, modifying obj2 will not affect obj1
+*/
