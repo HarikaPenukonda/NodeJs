b8c4c3658e71683078dd769322ad8a4283360556
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 17:59:57 -0500

    pipe-chaining and gzip

--- built-in-modules/streams/index.js
+++ built-in-modules/streams/index.js
@@ -1,9 +1,14 @@
 const fs = require("node:fs")
+const zlib = require("node:zlib") // allows us to use zip files
+
+const gzip = zlib.createGzip()
 
 const readableStream = fs.createReadStream("./file1.txt",{
     encoding:"utf-8",
     highWaterMark : 2
 })
+
+readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz")) // chaining with pipe
 
 const writableStream = fs.createWriteStream("./file2.txt")
 readableStream.pipe(writableStream)
