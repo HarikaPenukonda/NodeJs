05ac161ad55b1f7bd0ba466dbb0e701afdabcf87
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 17:47:10 -0500

    highWaterMark property

--- built-in-modules/streams/index.js
+++ built-in-modules/streams/index.js
@@ -1,7 +1,8 @@
 const fs = require("node:fs")
 
 const readableStream = fs.createReadStream("./file1.txt",{
-    encoding:"utf-8"
+    encoding:"utf-8",
+    highWaterMark : 2
 })
 
 const writableStream = fs.createWriteStream("./file2.txt")
