3f93d0fdc817e3a94d464d52667da4ae0c8d228c
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 16:07:29 -0500

    passing parameters to the event

--- built-in-modules/events/index.js
+++ built-in-modules/events/index.js
@@ -1,1 +1,11 @@
-const EventEmiiter = require('node:events')
+const EventEmitter = require('node:events') // returns a class EventEmitter
+
+const emitter = new EventEmitter()
+
+// register a listener
+emitter.on("order-pizza",(size,topping)=>{
+    console.log(`order-recieved!, Baking a ${size} pizza with ${topping}`)
+})
+
+emitter.emit("order-pizza","large","mushroom")
+
