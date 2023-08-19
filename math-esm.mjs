75ee762cfcdd60f3a76604a7914183a9a83e4459
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 14:17:01 -0500

    ES module import-export pattern 3

--- math-esm.mjs
+++ math-esm.mjs
@@ -1,5 +1,16 @@
-export default (a,b) => {
-    return a + b
-}
+// export default (a,b) => {
+//     return a + b
+// }
 
 //export default add
+
+const add = (a,b) =>{
+    return a+b
+}
+
+const subract = (a,b) =>{
+    return a-b
+}
+
+export default{add,subract}
+
