1bc52e5d7286ceadb3696a35389aa2094f66a2a2
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 17:22:29 -0500

    sync vs async

--- built-in-modules/fs-module/index.js
+++ built-in-modules/fs-module/index.js
@@ -1,9 +1,12 @@
 const fs = require("node:fs")
+console.log("first")
 
 // read the contents of a file, the fs module internally uses buffer
 //readFileSync is synchronous way of reading a file
 const fileContents = fs.readFileSync("/Users/matthewhinthorn/Desktop/NodeProjects/NodeJS/file.txt","utf-8")
 console.log(fileContents) 
+
+console.log("second")
 
 // for asynchronous way of reading a file
 fs.readFile("/Users/matthewhinthorn/Desktop/NodeProjects/NodeJS/file.txt","utf-8",(error,data)=>{
@@ -13,3 +16,5 @@ fs.readFile("/Users/matthewhinthorn/Desktop/NodeProjects/NodeJS/file.txt","utf-8
         console.log(data)
     }
 })
+
+console.log("third")
