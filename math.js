6ee200dc4dc637857fb2d7eeaf87e625509d7f97
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 13:37:41 -0500

    module exports vs exports explanation

--- math.js
+++ math.js
@@ -9,7 +9,12 @@ const subract = (a,b) => {
 exports = {add,subract} // TypeError: add is not a function
 
 /*
-    from a module Node.js only returns module.exports but not exports object
+    from a module Node.js only returns module.exports but not exports object, exports is just an reference to module.exports
+    If we assign a new object to exports, that reference is broken and module.exports will be an empty object that is exported
+    
+    
+    In index.js math becomes an empty object, if we access add, subrat from empty object we get error
+    TypeError: add is not a function
 */
 
 // exports.add  = (a,b) =>{
