b172e188af1eb11019ab6e7cc25f7fc00c297c84
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 13:31:43 -0500

    module.exports

--- math.js
+++ math.js
@@ -1,10 +1,19 @@
-exports.add  = (a,b) =>{
-    return a+b
+const add = (a,b) => {
+    return a + b
 }
 
+const subract = (a,b) => {
+    return a - b
+}
 
-exports.subract  = (a,b) =>{
-    return a-b
-} 
+module.exports = {add,subract}
+
+// exports.add  = (a,b) =>{
+//     return a+b
+// }
+
+// exports.subract  = (a,b) =>{
+//     return a-b
+// } 
 
 //module.exports = {add,subract}
