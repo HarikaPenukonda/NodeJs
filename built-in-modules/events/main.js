d2975516111432cd3eb726117d0fdbffb06f5855
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 16:42:17 -0500

    drink-machine class and methods

--- built-in-modules/events/main.js
+++ built-in-modules/events/main.js
@@ -1,9 +1,12 @@
 const PizzaShop  = require("./pizza-shop")
+const DrinkMachine = require("./drink-machine")
 
 const pizzaShop = new PizzaShop()
+const drinkMachine = new DrinkMachine()
 
 pizzaShop.on("order",(size,topping)=>{
     console.log(`order-recieved!, Baking a ${size} pizza with ${topping}`)
+    drinkMachine.serveDrink(size)
 })
 
 pizzaShop.order("large","pineapple")
