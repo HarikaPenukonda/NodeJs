ed66e88feccacc4ba897cd3620a92c01ed33f658
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 13:00:57 -0500

    Module - caching

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-require('./batman')
-require('./superman')
+const superHero = require('./super-hero') // loaded and cached(remembered)
+console.log(superHero.getName()); // Batman
+
+superHero.setName("Harry Potter")
+console.log(superHero.getName()); // HarryPotter
+
+const newSuperHero = require('./super-hero') // create another instance
+console.log(newSuperHero.getName()); // HarryPotter
+
+/*
+    we are creating new instance of super hero which should recieve batman as the name
+    module caching
+    when we require new module, it is loaded and cached(remembered) for subsequent loading
+    
+*/
+
+
+
+// require('./batman')
+// require('./superman')
 
 // const addFn = require('./add')
 // console.log("hello from index.js")
