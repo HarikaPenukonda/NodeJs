233d5c63be2d5558e7b2146f0e0895c080d626c9
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-19 17:36:44 -0500

    promises vs async-await

---
+++ built-in-modules/fs-module/fs-promise/index.js
@@ -0,0 +1,21 @@
+const { error } = require("node:console")
+const fs = require("node:fs/promises")
+
+async function readFile(){
+    try{
+        const data = await fs.readFile("/Users/matthewhinthorn/Desktop/NodeProjects/NodeJS/file.txt", "utf-8")
+        console.log(data)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+// console.log("first")
+
+// fs.readFile("/Users/matthewhinthorn/Desktop/NodeProjects/NodeJS/file.txt","utf-8")
+// .then(data => console.log(data))
+// .catch(error => console.log(error,"No such file"))
+
+//console.log("second")
+
+readFile()
