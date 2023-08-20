24eaaf08b7e34bd97d05411c43ecba49c72098bb
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-20 18:51:18 -0500

    Async pattern - Node's Native Option

--- built-in-modules/fs-module/fs-promise/app.js
+++ built-in-modules/fs-module/fs-promise/app.js
@@ -1,25 +1,14 @@
-const {readFile} = require('fs')
+const {readFile, writeFile} = require('fs')
+const util = require('util')
 
-const getText = (path) =>{
-    return new Promise((resolve,reject)=>{
-        readFile(path,"utf8",(err,data)=>{
-            if(err){
-                reject(err)
-            }else{
-                resolve(data)
-            }
-        })
-    })
-}
-
-// getText("./file.txt")
-// .then((result)=>console.log(result))
-// .catch(err => console.log(err))
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
 const start = async() =>{
     try{
-        const first = await getText("./first.txt")
-        const second = await getText("./second.txt")
+        const first = await readFilePromise("./first.txt",'utf8')
+        const second = await readFilePromise("./second.txt",'utf8')
+        await writeFilePromise('./result.txt-async.txt',`THIS IS AWESOME : ${first} , ${second}`)
         console.log(first,second)
     }catch(err){
         console.log(err)
@@ -28,3 +17,20 @@ const start = async() =>{
 }
 
 start()
+
+// const getText = (path) =>{
+//     return new Promise((resolve,reject)=>{
+//         readFile(path,"utf8",(err,data)=>{
+//             if(err){
+//                 reject(err)
+//             }else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText("./file.txt")
+// .then((result)=>console.log(result))
+// .catch(err => console.log(err))
+
