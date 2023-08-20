816f724f5ae860938ac940a6edf79afe3377ab33
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-20 18:19:18 -0500

    micro-queue

--- event-loops/index.js
+++ event-loops/index.js
@@ -1,8 +1,14 @@
-console.log("statement-1")
-process.nextTick(()=>console.log("this is process.next 1"))
-console.log("statement-2")
+// console.log("statement-1")
+// process.nextTick(()=>console.log("this is process.next 1"))
+// console.log("statement-2")
 
 // sync is given priority over async
 // statement-1
 // statement-2
 // this is process.next 1
+
+
+Promise.resolve().then(()=>console.log("this is Promise.resolve 1"))
+process.nextTick(()=>console.log("this is process.next 1"))
+// this is process.next 1
+// this is Promise.resolve 1
