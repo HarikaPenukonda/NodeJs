2e401b86e7fdbf2385b9f78d2284d1f4b298af2d
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-20 12:47:33 -0500

    Notes

--- built-in-modules/http-module/http-routing.js
+++ built-in-modules/http-module/http-routing.js
@@ -25,3 +25,15 @@ const server = http.createServer((request,response)=>{
 }) 
 
 server.listen(3000,()=>console.log("server running on port 3000.."))
+
+/*
+    This is how we can map requests to the appropriate response in code
+
+    The http module gives us access to the request object which in turn gives us access to the URL property
+
+    request. also gives us access to the HTTP method such as GET,PUT,POST,DELETE which we can use to route different requests
+
+    A combinatation of request.url and request. method can help us handle any type of routing with http module
+
+    However, In real world app we typically rely on a web framework to handle all such request for us
+*/
