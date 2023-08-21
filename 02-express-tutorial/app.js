1b4d709bba329f91dace61bca667eec02ab6f184
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-21 12:34:09 -0500

    external file html

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,19 @@
 const http = require('http')
+const {readFileSync} = require('fs')
+
+
+const homePage = readFileSync('./index.html')
 
 const server = http.createServer((request,response)=>{
-    response.writeHead(200,{'content-type' : 'text/html'})
-    response.write('<h1>My Home Page</h1>')
-    response.end()
+    //console.log(request.method) // GET
+    //console.log(request.url)
+    const url = request.url
+    if(url === '/home'){
+        response.writeHead(200,{'content-type' : 'text/html'})
+        response.write(homePage)
+        response.end()
+    }
+    
 })
 server.listen(3000,()=>console.log("Server listening at port 3000..."))
 // port is a communication endpoint
