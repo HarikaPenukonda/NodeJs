4f6bacab7fc6c3669dd225abe0ea430f44f7e5e7
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-21 10:14:31 -0500

    passing arguments

--- built-in-modules/events/app.js
+++ built-in-modules/events/app.js
@@ -2,8 +2,18 @@ const EventEmitter = require('node:events')
 
 const customeEmitter = new EventEmitter()
 
-customeEmitter.on('response',()=>{
-    console.log(`data recieved`)
+customeEmitter.on('response',(name,age)=>{
+    console.log(`data recieved ${name} and ${age}`)
+    
 })
 
-customeEmitter.emit('response')
+customeEmitter.on('response',()=>{
+    console.log(`some other logic here`)
+})
+
+customeEmitter.emit('response','harry',28)
+
+/*
+    1. we can use any number of functions, that are listening for the events
+    2. The order matters, first listen for event then emit
+*/
