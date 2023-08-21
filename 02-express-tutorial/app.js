685713f3b5e07bdf24980a74783be2cb9d2d03aa
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-21 12:07:58 -0500

    response - write and writeHead method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,9 @@
 const http = require('http')
 
 const server = http.createServer((request,response)=>{
-    console.log('user hit the server')
-    response.end("Home Page")
+    response.writeHead(200,{'content-type' : 'text/html'})
+    response.write('<h1>My Home Page</h1>')
+    response.end()
 })
 server.listen(3000,()=>console.log("Server listening at port 3000..."))
 // port is a communication endpoint
