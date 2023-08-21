d3dc8bae71cbd3a446fbdf09a800f547a0f4684f
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-21 12:02:14 -0500

    response end method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((request,response)=>{
+    console.log('user hit the server')
+    response.end("Home Page")
+})
+server.listen(3000,()=>console.log("Server listening at port 3000..."))
+// port is a communication endpoint
