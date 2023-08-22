a09c5fae08a86a3b6f24ed1c3da7cf25a953f870
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-21 22:16:28 -0500

    get products

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,7 +3,16 @@ const {products} = require('./data')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.json(products)
+    // res.json(products)
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
+
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((data)=>{
+        const {id,name,image} = data
+        return {id,name,image}
+    })
+    res.json(newProducts)
 })
 
 app.listen(3000,()=>console.log("server is listening to port 3000..."))
