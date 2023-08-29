7016e7c0aa01b7800add1852dcf8fc32263a0d8e
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-29 14:26:14 -0500

    link to products

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,13 +2,13 @@ const express = require('express')
 const {products} = require('./data')
 const app = express()
 
-// app.get('/',(req,res)=>{
-//     res.json(products)
-// })
+
 app.get('/',(req,res)=>{
-    res.json([{name:'admin'},{name:'guest'}])
+    res.send('<h1>Hello World</h1><a href="/api/products">products</a>')
 })
 
-
+app.get('/api/products',(req,res)=>{
+    res.json(products)
+})
 
 app.listen(3001,()=>console.log("server is listening to port 3001..."))
