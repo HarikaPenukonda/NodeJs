00e2c1ac9388c02b4ae1eb554bef94faf2bb9c27
HarikaPenukonda <penukondaharika@gmail.com>
2023-08-29 15:12:34 -0500

    no products matched

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -38,6 +38,10 @@ app.get('/api/v1/query',(req,res)=>{
     if(limit){
         sortedProducts = sortedProducts.slice(0,Number(limit))
     }
+    if(sortedProducts.length<1){
+        //res.status(200).send('no products matched')
+        res.status(200).json({success:true,data:[]})
+    }
     res.status(200).json(sortedProducts)
     //res.send('Hello World')
 })
