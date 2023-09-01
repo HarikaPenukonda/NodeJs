3101a4757df220a75f361ce2af7efafd21a17c36
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-01 14:41:29 -0500

    GET method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,11 @@
 const express = require('express')
 const app = express()
-const logger = require('./logger')
-const authorize = require('./authorize')
-const morgan = require('morgan')
 
-// req => middleware => res
-// app.use([logger,authorize])
-//app.use(express.static('./public'))
-app.use(morgan('tiny'))
+const {people} = require('./data')
 
-app.get('/',(req,res)=>{
-    res.send('Home')
+app.get('/api/people',(req,res)=>{
+    //res.status(200).send(people)
+    res.status(200).json({sucess : true,data:people})
 })
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
-app.get('/api/products',(req,res)=>{
-    res.send('Products')
-})
-app.get('/api/items',(req,res)=>{
-    console.log(req.user)
-    res.send('Items')
-})
-
 
 app.listen(3001,()=>console.log("server is listening to port 3001..."))
